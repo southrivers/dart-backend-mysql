66627023f9655692501c81594b5b8dd53eaa9466
wangershuai <wanganlou@gmail.com>
2024-06-15 22:33:02 +0800

    完成聊天功能的开发

--- lib/handler/chat_handler.dart
+++ lib/handler/chat_handler.dart
@@ -1,26 +1,18 @@
-import 'dart:io';
-
-import 'package:shelf/shelf.dart';
-import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-Future<Response> chat(Request request) async {
-  final _client = <WebSocketChannel>[];
-  final httpRequest =
-      request.context['shelf.io.request'] as HttpRequest;
-  if (WebSocketTransformer.isUpgradeRequest(httpRequest)) {
-    var socket = await WebSocketTransformer.upgrade(httpRequest);
-    var channel = IOWebSocketChannel(socket);
-    _client.add(channel);
-    channel.stream.listen((event) {
-      for (var value in _client) {
-        value.sink.add('feed back: ${event.toString()}');
+class ChatHandler {
+  var client = <WebSocketChannel>[];
+
+  Future<void> chatMsg(WebSocketChannel webSocketChannel) async {
+    client.add(webSocketChannel);
+    webSocketChannel.stream.listen((event) {
+      for (var value in client) {
+        if (value != webSocketChannel) {
+          value.sink.add(event);
+        }
       }
     }, onDone: () {
-      _client.remove(channel);
+      client.remove(webSocketChannel);
     });
-    return Response.ok('connection is established');
-  } else {
-    return Response.notFound('not a web socket');
   }
 }
--- lib/router/route.dart
+++ lib/router/route.dart
@@ -3,11 +3,12 @@ import 'package:server_learning/handler/chat_handler.dart';
 import 'package:server_learning/handler/home_handler.dart';
 import 'package:server_learning/handler/login_handler.dart';
 import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf_web_socket/shelf_web_socket.dart';
 
 Router createRoute() {
   var route = Router();
   route.get('/home', homeHandler);
   route.post('/login', login);
-  route.get('/chat', chat);
+  route.get('/chat', webSocketHandler(ChatHandler().chatMsg));
   return route;
 }
